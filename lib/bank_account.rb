ab413de7e67f90d609ebdd3b541ffa06771364d3
Oluwafunmilayo Adewodu <funmiadewodu@yahoo.com>
2017-09-06 13:17:04 +0100

    refactoring deposit and withdrawal methods

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,31 +5,27 @@ require_relative 'printer'
 class BankAccount
   INITIAL_BALANCE = 0.0
 
-  attr_reader :transactions, :credit_class, :debit_class
+  attr_reader :account_balance, :transactions, :credit_class, :debit_class
 
   def initialize(printer = Printer.new, credit_class = Credit, debit_class = Debit)
     @printer = printer
     @credit_class = credit_class
     @debit_class = debit_class
-    @balance = INITIAL_BALANCE
+    @account_balance = INITIAL_BALANCE
     @created_at = Time.now
     @transactions = []
   end
 
-  def account_balance
-    @balance
-  end
-
   def make_deposit(amount)
-    @balance += amount
-    credit = credit_class.new(amount)
-    @transactions << { transaction: credit, balance: @balance }
+    @account_balance += amount
+    credit = credit_class.new(amount, account_balance)
+    transactions << { transaction: credit, balance: account_balance }
   end
 
   def make_withdrawal(amount)
-    @balance -= amount
-    debit = debit_class.new(amount)
-    @transactions << { transaction: debit, balance: @balance }
+    @account_balance -= amount
+    debit = debit_class.new(amount, account_balance)
+    transactions << { transaction: debit, balance: account_balance }
   end
 
   def sort_by_date(transactions)
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -7,9 +7,10 @@ describe BankAccount do
   let(:bank_account) { described_class.new(printer, credit_class, debit_class) }
   let(:credit) { double("Credit", amount: 10.0, date_created: "04/09/2017") }
   let(:debit) { double("Debit", amount: -5.0, date_created: "05/09/2017") }
+  let(:credit_amount) { 15.00 }
+  let(:debit_amount) { 10.00 }
 
   context "when creating a new account" do
-
     it "has an initial balance of zero" do
       expect(bank_account.account_balance).to equal(0.0)
     end
@@ -19,34 +20,38 @@ describe BankAccount do
     end
   end
 
-  context "when making a deposit" do
-    before do
-      allow(credit_class).to receive(:new).with(10.00)
-    end
+  describe "make_deposit" do
+    let(:current_balance) { 15.00 }
 
-    it "increases the balance by that amount" do
-      expect { bank_account.make_deposit(10.00) }.to change { bank_account.account_balance }.by(10.00)
-    end
+    context "when making a deposit" do
+      before do
+        allow(credit_class).to receive(:new).with(credit_amount, current_balance)
+      end
 
-    it "creates a transaction" do
-      expect { bank_account.make_deposit(10.00) }.to change { bank_account.transactions.count }.by(1)
+      it "increases the balance by that amount" do
+        expect { bank_account.make_deposit(credit_amount) }.to change { bank_account.account_balance }.by(15.00)
+      end
+
+      it "creates a transaction" do
+        expect { bank_account.make_deposit(credit_amount) }.to change { bank_account.transactions.count }.by(1)
+      end
     end
   end
 
-  context "when making a withdrawal" do
-    before do
-      allow(credit_class).to receive(:new).with(15.00)
-      bank_account.make_deposit(15.00)
-      allow(debit_class).to receive(:new).with(10.00)
-    end
+  describe "make_withdrawal" do
+    let(:increased_balance) { 15.00 }
+    let(:current_balance) { 5.00 }
 
-    it "decreases the balance by that amount" do
-      expect { bank_account.make_withdrawal(10.00) }.to change { bank_account.account_balance }.by(-10.00)
-    end
+    context "when making a withdrawal" do
+      before do
+        allow(credit_class).to receive(:new).with(credit_amount, increased_balance)
+        bank_account.make_deposit(credit_amount)
+        allow(debit_class).to receive(:new).with(debit_amount, current_balance)
+      end
 
-    it "has a new balance" do
-      bank_account.make_withdrawal(10.00)
-      expect(bank_account.account_balance).to equal(5.00)
+      it "decreases the balance by that amount" do
+        expect { bank_account.make_withdrawal(debit_amount) }.to change { bank_account.account_balance }.by(-10.00)
+      end
     end
   end
 
